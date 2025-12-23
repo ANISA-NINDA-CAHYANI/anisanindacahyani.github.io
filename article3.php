fd1f4de45e5d90fd8b9c97466eaddf066322d332
ANISA-NINDA-CAHYANI <anisa0701232074@uinsu.ac.id>
2025-12-23 12:21:33 +0700

    Update article3.php

--- article3.php
+++ article3.php
@@ -7,7 +7,7 @@
 <body>
 <div id="wrapper">
     <div id="container">
-        <header><img src="Header.png"></header>
+        <header><img src="header.png"></header>
 		<nav>
 		<ul>
 			<li><a href="index.php">Beranda</a></li>
@@ -119,4 +119,5 @@
 </div>
 </body>
 </html>
+
 
